2ca35a28ab4a474954abf84f58dddb1a6e52b778
zhx <zhx12050128@163.com>
2023-04-09 18:18:23 +0800

    创建空项目

---
+++ src/test/java/com/springboot/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
